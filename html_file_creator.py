7d4fe6e785ee9e2d3ed224c13d8d258bdb254672
Annikura <annikura@yandex.ru>
2016-04-25 18:48:00 +0300

    Quite working program. Generates a simple html file. As for the changes, the html_file_creator.py was finished(several functions added), processing.py was modified by changing a type of qoutes, the text for the html file was added to variables.py

--- html_file_creator.py
+++ html_file_creator.py
@@ -13,17 +13,40 @@ def add_head(doc, title, css_file="None"):
     doc.append("</head>")
 
 
+def insert_row(doc, cnt, content):
+    doc.append("<tr>")
+    for i in range(cnt):
+        doc.append("<td>" + str(content[i]) + "</td>")
+    doc.append("</tr>")
+
+
+def insert_table(doc, rows, cols, titles, content, align = "center",
+                 border = "1", caption=""):
+    doc.append("<table" + ' align="' + align + '" border="' + border + '" >')
+    if caption != "":
+        doc.append("<caption><h2>" + caption + "</h2></caption>")
+    insert_row(doc, cols, [elem for elem in titles])
+    for i in range(rows):
+        insert_row(doc, cols, [elem(i) for elem in content])
+    doc.append("</table>")
+
+
 def output(max_length, column_titles, column_content):
-    file = open('results.html', 'w')
+    file = open("results.html", 'w')
     filecontent = []
     if len(column_titles) != len(column_content):
-        print('There are different counts of titles and contents')
+        print("There are different counts of titles and contents")
         return
     num_of_col = len(column_titles)
     num_of_row = min(max_length, variables.rows_to_show)
 
-    filecontent.append('<html>')
-    add_head(filecontent, 'Result table') #, 'table_style.css')
+    filecontent.append("<html>")
+    add_head(filecontent, 'Result table')  # , 'table_style.css')
+    filecontent.append("<body>")
 
-    filecontent.append('</html>')
-    file.writelines(filecontent)
+    filecontent.append("<p>" + variables.text + "</p>")
+    insert_table(filecontent, num_of_row, num_of_col, column_titles, column_content,
+                 caption="Result table")
+    filecontent.append("<body>")
+    filecontent.append("</html>")
+    file.writelines([s + '\n' for s in filecontent])
--- variables.py
+++ variables.py
@@ -1,11 +1,17 @@
-#filtration parameters
+# filtration parameters
 
 lowest_age = 20
 highest_age = 42
 lowest_score = 20
 
-#show parameters
+# show parameters
 
 rows_to_show = 50
+
+text = "Here is the result file.\n Below you can find a table, " \
+       "which contains a list of first " + str(rows_to_show) + " users from " + \
+       str(lowest_age) + " to " + str(highest_age) + " years old, " \
+       "who have the biggest amounts of comments to posts " \
+       "with score more than " + str(lowest_score) + '\n'
 
 url = 'http://electronics.stackexchange.com/'
