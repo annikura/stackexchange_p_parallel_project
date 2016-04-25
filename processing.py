2aa8ac6d80890ff2e260e5f3043736bdefe4bec1
Annikura <annikura@yandex.ru>
2016-04-25 16:50:35 +0300

    processing.py is finished and settings were put into a separate file

--- processing.py
+++ processing.py
@@ -1,41 +1,71 @@
+import variables
 from file_parser import file_content
 from filter import filtered_data
-
-lowest_age = 20
-highest_age = 42
-lowest_score = 20
+from html_file_creator import output
+from html_file_creator import link_maker
 
 users_data = file_content('Users.xml')
 posts_data = file_content('Posts.xml')
 comments_data = file_content('Comments.xml')
 
 filtered_users = filtered_data(users_data, 'Id',
-                             [
-                                 'Age'
-                             ],
-                             [
-                                 lambda age: lowest_age <= int(age) <= highest_age
-                             ]
-                             )
+                               [
+                                   'Age'
+                               ],
+                               [
+                                   lambda age: variables.lowest_age <= int(age) and
+                                               int(age) <= variables.highest_age
+                               ]
+                               )
 filtered_posts = filtered_data(posts_data, 'Id',
-                             [
-                                 'Score'
-                             ],
-                             [
-                                 lambda score: lowest_score < int(score)
-                             ]
-                             )
+                               [
+                                   'Score'
+                               ],
+                               [
+                                   lambda score: variables.lowest_score < int(score)
+                               ]
+                               )
 filtered_comments = filtered_data(comments_data, '__index',
-                                [
-                                    'PostId',
-                                    'UserId'
-                                ],
-                                [
-                                    lambda post_id: post_id in filtered_posts,
-                                    lambda user_id: user_id in filtered_users
-                                ]
-                                )
+                                  [
+                                      'PostId',
+                                      'UserId'
+                                  ],
+                                  [
+                                      lambda post_id: post_id in filtered_posts,
+                                      lambda user_id: user_id in filtered_users
+                                  ]
+                                  )
 
 users_id_to_index = dict()
+
 for elem in users_data:
     users_id_to_index[elem['Id']] = elem['__index']
+
+comments_counter = dict()
+users_to_output = []
+
+for index in filtered_comments:
+    id = users_id_to_index[comments_data[index]['UserId']]
+    if comments_counter.get(id) is None:
+        comments_counter[id] = 0
+        users_to_output.append(id)
+    comments_counter[id] += 1
+users_to_output.sort(key=lambda i: comments_counter[i], reverse=True)
+
+output(
+    len(users_to_output),
+    [
+        '#',
+        'Id',
+        'Link',
+        'Comments'
+    ],
+    [
+        lambda i: i + 1,
+        lambda i: users_data[users_to_output[i]]['Id'],
+        lambda i: link_maker('http://electronics.stackexchange.com/users/' +
+                             users_data[users_to_output[i]]['Id'],
+                             users_data[users_to_output[i]]['DisplayName']),
+        lambda i: comments_counter[users_to_output[i]]
+    ]
+)
---
+++ variables.py
@@ -0,0 +1,6 @@
+#filtration parameters
+
+lowest_age = 20
+highest_age = 42
+
+lowest_score = 20
