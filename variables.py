6ecfa3e17b288200567cc4c32e3a3505329c0b8a
annikura <annikura@yandex.ru>
2016-04-26 01:07:08 +0300

    Changed 'lower' and 'higher' to 'min' and 'max'

--- variables.py
+++ variables.py
@@ -3,9 +3,9 @@
 
 # filtration parameters
 
-lowest_age = 20
-highest_age = 42
-lowest_score = 20
+min_age = 20
+max_age = 42
+min_score = 20
 
 # output file parameters
 
@@ -13,9 +13,9 @@ rows_to_show = 50
 
 text = "Here is the result file.\n Below you can find a table, " \
        "which contains a list of first " + str(rows_to_show) + " users from " + \
-       str(lowest_age) + " to " + str(highest_age) + " years old, " \
+       str(min_age) + " to " + str(max_age) + " years old, " \
        "who have the biggest amounts of comments to posts " \
-       "with score more than " + str(lowest_score) + '\n' + "To change the number of the " \
+       "with score more than " + str(min_score) + '\n' + "To change the number of the " \
        "table rows, change the variable 'rows_to_show' in 'variables.py' file\n" + \
        "To change the age or score filters, change variables 'lowest_age', " \
        "'highest_age' or 'lowest_score' in the same file\n"
