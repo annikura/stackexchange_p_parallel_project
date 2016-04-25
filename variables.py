838c7436c3b7f8aa46bf87708066c78f20cc4c09
annikura <annikura@yandex.ru>
2016-04-26 01:38:31 +0300

    text fixes

--- variables.py
+++ variables.py
@@ -17,8 +17,8 @@ text = "Here is the result file.\n Below you can find a table, " \
        "who have the biggest amounts of comments to posts " \
        "with score more than " + str(min_score) + '\n' + "To change the number of the " \
        "table rows, change the variable 'rows_to_show' in 'variables.py' file\n" + \
-       "To change the age or score filters, change variables 'lowest_age', " \
-       "'highest_age' or 'lowest_score' in the same file\n"
+       "To change the age or score filters, change variables 'min_age', " \
+       "'max_age' or 'min_score' in the same file\n"
 
 # a link of a site to redirect to. (where the users are situated)
 
